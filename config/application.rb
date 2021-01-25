fd281306568c192bc0b85be033e23ca4cd2e4123
K-Arai-tsuhiro <arai.katsuhiro@gmail.com>
2021-01-25 17:53:36 +0900

    make sure

---
+++ config/application.rb
@@ -0,0 +1,33 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TechpitMatch
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    config.time_zone = "Tokyo"
+
+    config.active_record.default_timezone =:local
+
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.javascripts false
+      g.helper false
+      g.test_framwork false
+    end
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root "top#index"
+  resources :users, only: %i(show index)
+  resources :reactions, only: %i(create)
+  resources :matching, only: %i(index)
+  resources :chat, only: %i(create show)
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
